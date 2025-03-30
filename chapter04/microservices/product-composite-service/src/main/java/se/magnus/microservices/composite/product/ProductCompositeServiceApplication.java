d6c867d13b163f353aac7a1b95affa737c04a984
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-03-30 17:49:44 +0900

    commit

--- chapter04/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ chapter04/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductCompositeServiceApplication {
-
+	// TODO : Docker 실행 후 curl 로 찔러보아야 함 !
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
 	}
-
 }
